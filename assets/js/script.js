d0848530575e866bfc446f3ed0078b319e4f9e77
BarbierTheo <lebarbier.lh@gmail.com>
2025-02-23 03:53:02 +0100

    travail sur le mnu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,129 @@
 // Vérifie si la page est chargé, sinon marche pas
-document.addEventListener('DOMContentLoaded', () => {
-
-  let state = ''
-  let open = false
+document.addEventListener("DOMContentLoaded", () => {
+  let state = "";
+  let open = false;
 
   // Ouverture menu hamburger
-  document.getElementById('openMenu').addEventListener('change', () => {
+  document.getElementById("openMenu").addEventListener("change", () => {
 
-    document.getElementById('menuInterface').classList.toggle('z-40')
-    document.getElementById('menuInterface').classList.toggle('op0')
-    document.getElementById('menuInterface').classList.toggle('op1')
+    const menuInterface = document.getElementById("menuInterface");
 
-    let delay = 600
+    let delay = 400;
 
-    for (let i = 1; i <= 4; i++) {
+    if (open) {
+      document.querySelector(".secondstate").innerHTML = ''
+      for (let i = 1; i <= 4; i++) {
+        document
+          .querySelector(`.entryMenu${i}`)
+          .classList.remove(
+            "animate-fade-right",
+            `animate-duration-[500ms]`,
+            "animate-ease-in-out",
+            "opacity-0"
+          );
+        document.querySelector(
+          `.entryMenu${i}`
+        ).style.animationDelay = `${delay}ms`;
+        document.querySelector(
+          `.entryMenu${i}`
+        ).style.opacity = `0`;
+        delay = delay + 250;
+      }
 
-      document.querySelector(`.entryMenu${i}`).classList.toggle('animate-fade-right', `animate-duration-[600ms]`, `animate-delay-[${delay}ms]`, 'animate-ease-in-out')
-      delay = delay + 200
-      console.log(document.querySelector(`.entryMenu${i}`))
+      menuInterface.style.opacity = "0";
+      setTimeout(() => {
+        menuInterface.classList.toggle("z-40");
+      }, 800);
+
+    } else {
+
+      for (let i = 1; i <= 4; i++) {
+        document
+          .querySelector(`.entryMenu${i}`)
+          .classList.add(
+            "animate-fade-right",
+            `animate-duration-[500ms]`,
+            "animate-ease-in-out",
+            "opacity-0"
+          );
+        document.querySelector(
+          `.entryMenu${i}`
+        ).style.animationDelay = `${delay}ms`;
+        document.querySelector(
+          `.entryMenu${i}`
+        ).style.opacity = `0`;
+        delay = delay + 100;
+      }
+
+      menuInterface.classList.toggle("z-40");
+      menuInterface.style.opacity = "1";
 
     }
 
-    open == true ? open = false : open = true
-    state = ''
 
-  })
+    open == true ? (open = false) : (open = true);
+    state = "";
 
-  // N'est pas toggle
-  document.getElementById('toggleServices').addEventListener('click', showServices)
+  });
+
+  document
+    .getElementById("toggleServices")
+    .addEventListener("click", showServices);
 
   // Ouverture menu services
   function showServices() {
-
-    if (state !== 'services') {
-      document.querySelector('.secondstate').innerHTML = `
-              <div class="w-[0.1rem] h-0 rounded-lg bg-gray-400></div>
+    if (state !== "services") {
+      document.querySelector(".secondstate").innerHTML = `
+              <div class="w-[0.1rem] rounded-lg bg-gray-400" style="height: 0; opacity: 0; transition: height 1s ease-in-out, opacity 1s ease-in-out;" id="barServices"></div>
               <div class="flex flex-col menu" id="menuServices" style="width : 0; white-space: nowrap; height: 0; opacity: 0; transition: width 1s ease, opacity 1s ease-in-out;">
                   <ul class="text-gray-400 font-bold text-xl lg:text-4xl flex flex-col gap-5 text-center lg:text-left">
                       <li><button class="hover:text-gray-600 transition-all duration-200">Architecture</button></li>
                       <li><button class="hover:text-gray-600 transition-all duration-200">Rendus & Visuels 3D</button></li>
                       <li><button class="hover:text-gray-600 transition-all duration-200">Conseils & accompagnements</button></li>
                   </ul>
-              </div>`
+              </div>`;
 
-      const menu = document.getElementById('menuServices')
+      const menu = document.getElementById("menuServices");
+      const bar = document.getElementById("barServices");
 
       setTimeout(() => {
+        menu.style.height = menu.scrollHeight + "px";
+        menu.style.opacity = "1";
+        menu.style.width = menu.scrollWidth + "px";
 
-        menu.style.height = menu.scrollHeight + 'px'
-        menu.style.opacity = '1'
-        menu.style.width = menu.scrollWidth + 'px';
+        bar.style.height = menu.scrollHeight + "px";
+        bar.style.opacity = "1";
+      }, 300);
 
-      }, 300)
-
-      state = 'services'
-
+      state = "services";
     } else {
+      const menu = document.getElementById("menuServices");
+      const bar = document.getElementById("barServices");
 
-      const menu = document.getElementById('menuServices')
+      menu.style.width = "0";
+      menu.style.opacity = "0";
 
-      menu.style.width = '0';
-      menu.style.opacity = '0';
+      bar.style.height = "0px";
+      bar.style.opacity = "0";
 
       setTimeout(() => {
-        document.querySelector('.secondstate').innerHTML = '';
-      }, 600);
+        document.querySelector(".secondstate").innerHTML = "";
+      }, 700);
 
-      state = ''
-
+      state = "";
     }
-
   }
 
-
-
-
-
-
-
-
-
   // Réglages carousel
-  const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
     loop: true,
     autoplay: {
       delay: 20000,
     },
-    effect: 'fade',
+    effect: "fade",
     fadeEffect: {
-      crossFade: true
+      crossFade: true,
     },
   });
-
-
-})
+});
--- menu.php
+++ menu.php
@@ -5,19 +5,21 @@
     </label>
 </header>
 
-<div class="op0 flex fixed h-screen w-screen bg-zinc-950/90 top-0 right-0 justify-center" id="menuInterface">
+<div class="flex fixed h-screen w-screen bg-zinc-950/90 top-0 right-0 justify-center" id="menuInterface" style="opacity: 1; transition: opacity 1s ease-in-out">
 
     <div class="flex flex-col lg:flex-row justify-center items-center gap-4 lg:gap-8">
         <div class="flex flex-col" id="menuList">
             <ul class="text-gray-100 font-bold text-2xl lg:text-4xl flex flex-col gap-5">
-                <li><button class="hover:text-gray-400 entryMenu1" id="toggleServices">Services</button></li>
-                <li><button class="hover:text-gray-400 entryMenu2" id="toggleProjects">Portfolio</button></li>
-                <li><a href="" class="hover:text-gray-400 entryMenu3 cursor-pointer">A propos</a></li>
-                <li><a href="" class="hover:text-gray-400 entryMenu4 cursor-pointer">Contact</a></li>
+                <li class="entryMenu1" style="opacity: 0;"><button class="hover:text-gray-400" id="toggleServices">Services</button></li>
+                <li class="entryMenu2" style="opacity: 0;"><button class="hover:text-gray-400" id="toggleProjects">Portfolio</button></li>
+                <li class="entryMenu3" style="opacity: 0;"><a href="" class="hover:text-gray-400 cursor-pointer">A propos</a></li>
+                <li class="entryMenu4" style="opacity: 0;"><a href="" class="hover:text-gray-400 cursor-pointer">Contact</a></li>
             </ul>
         </div>
 
-        <div class="secondstate flex gap-4">
+        <div class="secondstate flex flex-col lg:flex-row gap-8">
+
+           
         </div>
 
         <!-- <div class="w-1 h-70 rounded-lg bg-gray-400" id="projectsDivider"></div>
@@ -38,3 +40,5 @@
         <a href="" class="text-3xl lg:text-4xl text-gray-50 self-end"><i class="uil uil-instagram"></i></a>
     </div>
 </div>
+
+<div class="hidden animate-fade-right animate-duration-[600ms] animate-delay-[300ms] animate-ease-in-out w-[0.1rem] h-auto "></div>
